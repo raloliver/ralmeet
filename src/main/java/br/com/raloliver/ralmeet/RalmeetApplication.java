21ead7c9384aee54ea344aa1e424840e7c6de892
raloliver <contato@raloliver.com.br>
2021-09-12 07:54:09 +0100

    Add files to project

---
+++ src/main/java/br/com/raloliver/ralmeet/RalmeetApplication.java
@@ -0,0 +1,13 @@
+package br.com.raloliver.ralmeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RalmeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RalmeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/raloliver/ralmeet/RalmeetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.raloliver.ralmeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RalmeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
