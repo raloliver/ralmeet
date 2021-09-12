1f36313e75b3e7b91836a8c25a7709af079407ee
raloliver <contato@raloliver.com.br>
2021-09-12 12:11:55 +0100

    Finally run the project

--- api/src/main/java/br/com/raloliver/ralmeet/Application.java
+++ api/src/main/java/br/com/raloliver/ralmeet/Application.java
@@ -4,10 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RalmeetApplication {
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RalmeetApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
 }
