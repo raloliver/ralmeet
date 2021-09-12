5aa51d7bbf57741ea9cbd059f01586bf29141767
raloliver <contato@raloliver.com.br>
2021-09-12 21:29:35 +0100

    Add JDBC with mysql driver in docker

--- api/src/main/java/br/com/raloliver/ralmeet/RoomController.java
+++ api/src/main/java/br/com/raloliver/ralmeet/RoomController.java
@@ -2,18 +2,18 @@ package br.com.raloliver.ralmeet;
 
 import br.com.raloliver.ralmeet.api.facade.RoomsApi;
 import br.com.raloliver.ralmeet.api.model.Room;
+import java.util.concurrent.CompletableFuture;
+import javax.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
-import java.util.concurrent.CompletableFuture;
-
 @RestController
 public class RoomController implements RoomsApi {
-    @Override
-    public CompletableFuture<ResponseEntity<Room>> listRooms(@Valid Long id) {
-        Room room = new Room().id(1L).name("Room #0");
 
-        return CompletableFuture.supplyAsync(() -> ResponseEntity.ok(room));
-    }
+  @Override
+  public CompletableFuture<ResponseEntity<Room>> listRooms(@Valid Long id) {
+    Room room = new Room().id(1L).name("Room #0");
+
+    return CompletableFuture.supplyAsync(() -> ResponseEntity.ok(room));
+  }
 }
